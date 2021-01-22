e51f546db6ae3f05b519ebda95355c8ce5fe1708
Fatih Taşkıran <fatihtaskran@gmail.com>
2021-01-23 00:50:57 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id +" "+ musteri.Adi + " " + musteri.Soyadi + " " + musteri.Yas + " Bilgilerine sahip müşteri eklendi");
+        }
+        public void Liste(Musteri[] musteriler)
+        {
+            //foreach (Musteri customer in musteriler)
+            //{
+            //    Console.WriteLine(customer.Id);
+            //    Console.WriteLine(customer.Adi);
+            //    Console.WriteLine(customer.Soyadi);
+            //    Console.WriteLine(customer.Yas);
+            //    Console.WriteLine("****************");
+            //}
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                Console.WriteLine(musteriler[i].Id);
+                Console.WriteLine(musteriler[i].Adi);
+                Console.WriteLine(musteriler[i].Soyadi);
+                Console.WriteLine(musteriler[i].Yas);
+            }
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Yas + " Bilgilerine sahip müşteri silindi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 01234;
+            musteri1.Adi = "Fatih İlhami";
+            musteri1.Soyadi = "TAŞKIRAN";
+            musteri1.Yas = 25;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 01235;
+            musteri2.Adi = "Engin";
+            musteri2.Soyadi = "DEMİROĞ";
+            musteri2.Yas = 32;
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Ekle(musteri1);
+            musterimanager.Ekle(musteri2);
+            Musteri[] listeleme = new Musteri[]
+            { musteri1, musteri2};
+            musterimanager.Liste(listeleme);
+            musterimanager.Silme(musteri1);
+            musterimanager.Silme(musteri2);
+        }
+    }
+}
